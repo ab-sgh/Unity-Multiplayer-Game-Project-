455aae0ad9d44525b2f19039f5f1112aa0277aaa
abhinav <abhinavsingh99311@gmail.com>
2024-06-26 23:59:58 +0530

    Second Commit

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -8,6 +8,7 @@ public class Bullet : MonoBehaviour
     public GameObject bulletPrefab;
 
     public float bulletforce= 20f;
+    public LayerMask playerLayer;
     // Update is called once per frame
     void Update()
     {
@@ -19,9 +20,10 @@ public class Bullet : MonoBehaviour
     void Shoot()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.TransformPoint(Vector3.zero), firePoint.rotation);
+        bullet.layer = gameObject.layer;
     Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
     rb.AddForce(firePoint.up * bulletforce, ForceMode2D.Impulse);
-    Debug.Log($"Bullet spawned at position: {bullet.transform.position}");
+    Debug.Log($"Bullet layer: {bullet.layer} Player Layer: {gameObject.layer}");
 
     }
 }
